119bcab35e39d059729064bb3681311fcc7569fa
Hasaan <hasaanahmedrana@gmail.com>
2024-10-23 14:55:40 +0500

    Added Solution of ksusha the squirrel  and readme updated

---
+++ Codeforces/299-B.Ksusha the Squirrel.py
@@ -0,0 +1,14 @@
+def main():
+    n, k=map(int, input().split())
+    road = input()
+    rock = 0
+    for i in range(n):
+        if road[i] == '#':
+            rock += 1
+            if rock >= k:
+                print('NO')
+                return
+        if road[i] == ".":
+            rock = 0
+    print('YES')
+main()
