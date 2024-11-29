8398687e09f1acc47c3d081cc457b01aa0833c5b
Hasaan <hasaanahmedrana@gmail.com>
2024-11-29 19:15:06 +0500

    Moved filename.py to Codeforces folder

--- Codeforces/1878-A.How Much Does Daytona Cost.py
+++ Codeforces/1878-A.How Much Does Daytona Cost.py
@@ -1,7 +1,9 @@
 def main():
-    n,k = map(int,input().split())
-    x = list(map(int,input().split()))
-    return 'YES' if k in x else'NO'
+    n, k = map(int, input().split())
+    x = list(map(int, input().split()))
+    return 'YES' if k in x else 'NO'
+
+
 for _ in range(int(input())):
     print(main())
 
